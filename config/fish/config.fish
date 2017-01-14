76acb7898221730333b5722c84461b01c128962e
Miguel Molina <hi@mvader.me>
2017-01-14 19:28:10 +0100

    Move install to dotbot and setup more apps

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,6 @@
 set -gx GOPATH $HOME/go
-set -gx PATH $PATH $HOME/.local/bin $GOPATH/bin
+set -gx PATH $PATH $HOME/.local/bin $GOPATH/bin ~/.npm-global/bin
 set -gx CDPATH $CDPATH $GOPATH/src/github.com . .. $HOME
+
+set -g theme_date_format "+%a %H:%M"
+set -g theme_color_scheme "solarized-dark"
