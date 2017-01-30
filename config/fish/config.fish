373ac8c25265d1acc070cdb6738d04cd487740ac
Miguel Molina <hi@mvader.me>
2017-01-30 12:21:44 +0100

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,7 @@ set -gx CDPATH $CDPATH $GOPATH/src/github.com . .. $HOME
 
 set -g theme_date_format "+%a %H:%M"
 set -g theme_color_scheme "solarized-dark"
+
+function gocov
+        go test -coverprofile=cover.out $argv[1]; and go tool cover -html=cover.out; and rm cover.out
+end
