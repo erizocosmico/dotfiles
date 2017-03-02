f2b3a66da2c129ae54d8c31236e4720c87f20574
Miguel Molina <hi@mvader.me>
2017-03-02 13:17:39 +0100

    small improvements

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,11 @@
 set -gx GOPATH $HOME/go
 set -gx PATH $PATH $HOME/.local/bin $GOPATH/bin ~/.npm-global/bin ~/.cargo/bin ~/.telegram
-set -gx CDPATH $CDPATH $GOPATH/src/github.com . .. $HOME
+set -gx CDPATH $CDPATH . .. $GOPATH/src/github.com $GOPATH/src/gopkg.in $HOME
 
 set -g theme_date_format "+%a %H:%M"
+
+alias vi "nvim"
+alias vim "nvim"
 
 function gocov
         go test -coverprofile=cover.out $argv[1]; and go tool cover -html=cover.out; and rm cover.out
